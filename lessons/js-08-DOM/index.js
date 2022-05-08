95aaee223fe894411ba45f3577fafc6e54ea7a88
NYaremiy <n.yaremiy791@gmail.com>
2022-05-08 17:39:35 +0300

    Fixed bug

--- lessons/js-08-DOM/index.js
+++ lessons/js-08-DOM/index.js
@@ -8,7 +8,7 @@ listSong();
 // Створити HTML-сторінку з кнопкою "Відкрити" і модальним вікном. На модальном вікні повинен бути текст і кнопка "Закрити". Спочатку модальне вікно не відображається. При кліку на кнопку "Відкрити" з'являється модальне вікно, на кнопку "Закрити" — зникає.
 showModaleW();
 document.getElementById('js-form').addEventListener('submit', singIn);
-
+// Створити HTML-сторінку зі світлофором і кнопкою, яка перемикає світлофор на наступний колір
 document
   .getElementById('js-tarffic')
   .addEventListener('click', setTrafficColorActive);
